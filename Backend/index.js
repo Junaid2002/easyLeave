9eb0e0e863ba02fb21fd8be062c248d077145762
mohammadsaqulain751 <mohammadsaqulain751@gmail.com>
2025-04-08 07:51:57 +0000

    The backend work of authentication is done

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,26 @@
-import {app} from "./app.js"
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import AuthRouter from './Routes/AuthRouter.js';
+import ProductRouter from './Routes/ProductRouter.js';
+import dotenv from 'dotenv';
+import './Models/db.js';
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on port ${process.env.PORT}`);
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping', (req, res) => {
+    res.send('PONG');
 });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+
+export default app;
